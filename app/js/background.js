c6bd2597c759e6863ceeb18d8d4c08f854ff2c88
Guillaume Grossetie <ggrossetie@gmail.com>
2015-05-04 10:11:45 +0200

    Format code

--- app/js/background.js
+++ app/js/background.js
@@ -2,29 +2,29 @@ var enableRender = true;
 var matchesTabUrl = chrome.runtime.getManifest().content_scripts[0].matches;
 
 chrome.contextMenus.create({
-  "title":"Render selection",
-  "contexts":["selection"], "onclick":function (info, tab) {
+  "title": "Render selection",
+  "contexts": ["selection"], "onclick": function (info, tab) {
     var funcToInject = function () {
       var selection = window.getSelection();
       return (selection.rangeCount > 0) ? selection.toString() : '';
     };
     var jsCodeStr = ';(' + funcToInject + ')();';
     chrome.tabs.executeScript({
-      code:jsCodeStr,
-      allFrames:true
+      code: jsCodeStr,
+      allFrames: true
     }, function (selectedTextPerFrame) {
       if (chrome.runtime.lastError) {
         console.log('error:' + chrome.runtime.lastError.message);
       } else if ((selectedTextPerFrame.length > 0) && (typeof(selectedTextPerFrame[0]) === 'string')) {
         var selectedText = selectedTextPerFrame[0];
         chrome.tabs.create({
-          'url':chrome.extension.getURL("html/inject.html"),
-          'active':true
+          'url': chrome.extension.getURL("html/inject.html"),
+          'active': true
         }, function (tab) {
           var selfTabId = tab.id;
           chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
             if (changeInfo.status == "complete" && tabId == selfTabId) {
-              var tabs = chrome.extension.getViews({type:"tab"});
+              var tabs = chrome.extension.getViews({type: "tab"});
               // Get the latest tab opened
               tabs[tabs.length - 1].inject(selectedText);
             }
@@ -82,12 +82,12 @@ function enableDisableRender() {
 
 function refreshOptions() {
   chrome.storage.local.set({
-    'CUSTOM_ATTRIBUTES':localStorage['CUSTOM_ATTRIBUTES'],
-    'SAFE_MODE':localStorage['SAFE_MODE'],
-    'ALLOW_TXT_EXTENSION':localStorage['ALLOW_TXT_EXTENSION'],
-    'THEME':localStorage['THEME'],
-    'JS':localStorage['JS'],
-    'JS_LOAD':localStorage['JS_LOAD']
+    'CUSTOM_ATTRIBUTES': localStorage['CUSTOM_ATTRIBUTES'],
+    'SAFE_MODE': localStorage['SAFE_MODE'],
+    'ALLOW_TXT_EXTENSION': localStorage['ALLOW_TXT_EXTENSION'],
+    'THEME': localStorage['THEME'],
+    'JS': localStorage['JS'],
+    'JS_LOAD': localStorage['JS_LOAD']
   });
   var customThemeNames = JSON.parse(localStorage['CUSTOM_THEME_NAMES'] || '[]');
   if (customThemeNames.length > 0) {
@@ -100,15 +100,15 @@ function refreshOptions() {
     }
   }
   var customJavaScriptNames = JSON.parse(localStorage['CUSTOM_JS_NAMES'] || '[]');
-    if (customJavaScriptNames.length > 0) {
-      for (var j in customJavaScriptNames) {
-        var javaScriptName = customJavaScriptNames[j];
-        var javaScriptNameKey = 'CUSTOM_JS_' + javaScriptName;
-        var javaScriptObj = {};
-        javaScriptObj[javaScriptNameKey] = localStorage[javaScriptNameKey];
-        chrome.storage.local.set(javaScriptObj);
-      }
+  if (customJavaScriptNames.length > 0) {
+    for (var j in customJavaScriptNames) {
+      var javaScriptName = customJavaScriptNames[j];
+      var javaScriptNameKey = 'CUSTOM_JS_' + javaScriptName;
+      var javaScriptObj = {};
+      javaScriptObj[javaScriptNameKey] = localStorage[javaScriptNameKey];
+      chrome.storage.local.set(javaScriptObj);
     }
+  }
 }
 
 chrome.browserAction.onClicked.addListener(enableDisableRender);
