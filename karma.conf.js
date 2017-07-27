b5e77cc1e30ac253087157ab235cbdfd115b0487
GUILLAUME GROSSETIE <guillaume-externe.grossetie@erdf.fr>
2017-07-27 14:06:05 +0200

    Use Headless Chrome for automated testing

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine'],
+    files: [
+      'app/js/vendor/highlight.min.js',
+      'app/js/vendor/jquery.min.js',
+      'app/js/vendor/opal.js',
+      'app/js/vendor/asciidoctor.js',
+      'app/js/vendor/md5.js',
+      'app/js/asciidocify.js',
+      'app/js/renderer.js',
+      'spec/**/*.js'
+    ],
+    reporters: ['progress'],
+    port: 9876,  // karma web server port
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['ChromeHeadless'],
+    autoWatch: false,
+    singleRun: false, // Karma captures browsers, runs the tests and exits
+    concurrency: Infinity
+  })
+}
