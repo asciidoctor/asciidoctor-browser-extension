1652997fdc92d42ed6eb0b35151d02afb7ad7349
Guillaume Grossetie <g.grossetie@gmail.com>
2018-02-05 16:59:11 +0100

    ✨ Resolves #185, use stylesheet attribute to configure stylesheet (#198)

--- app/js/inject.js
+++ app/js/inject.js
@@ -3,7 +3,6 @@ let alreadyRun = false;
 // eslint-disable-next-line no-unused-vars
 function inject (source) {
   if (!alreadyRun) {
-    appendStyles();
     asciidoctor.browser.update(source);
     alreadyRun = true;
   }
