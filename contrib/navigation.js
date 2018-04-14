685a156eeeff360aa760ac07364da0fa62747847
Guillaume Grossetie <ggrossetie@gmail.com>
2018-04-14 12:54:00 +0200

    Use pure JavaScript

--- contrib/navigation.js
+++ contrib/navigation.js
@@ -1,24 +1,38 @@
-/**
- * Scroll to the next/previous section (<h2>) with keyboard up/down.
- */
-var headings = $('h1, h2, h3, h4, h5').not('.float');
-var current = 0;
-var headingsLength = headings.length;
-$(document).keydown(function (e) {
-  if (e.keyCode === 38) {
-    // up
-    if (current > 0) {
-      e.preventDefault();
-      current -= 1;
-      $('html, body').animate({scrollTop: headings[current].offsetTop}, 1000);
+// Scroll to the next/previous section with keyboard up/down.
+(function () {
+  var headings = Array.from(document.getElementsByTagName('h1'))
+    .concat(Array.from(document.getElementsByTagName('h2')))
+    .concat(Array.from(document.getElementsByTagName('h3')))
+    .concat(Array.from(document.getElementsByTagName('h4')))
+    .concat(Array.from(document.getElementsByTagName('h5')))
+    .filter(function (heading) {
+      return !Array.from(heading.classList).includes('float')
+    });
+  var current = 0;
+  var headingsLength = headings.length;
+
+  window.addEventListener("keydown", function (e) {
+    if (e.key === 'ArrowUp') {
+      // up
+      if (current > 0) {
+        e.preventDefault();
+        current -= 1;
+        window.scrollTo({
+          top: headings[current].offsetTop,
+          behavior: "smooth"
+        });
+      }
     }
-  }
-  else if (e.keyCode === 40) {
-    // down
-    if (current < headingsLength - 1) {
-      e.preventDefault();
-      current += 1;
-      $('html, body').animate({scrollTop: headings[current].offsetTop}, 1000);
+    else if (e.key === 'ArrowDown') {
+      // down
+      if (current < headingsLength - 1) {
+        e.preventDefault();
+        current += 1;
+        window.scrollTo({
+          top: headings[current].offsetTop,
+          behavior: "smooth"
+        });
+      }
     }
-  }
-});
+  });
+})();
