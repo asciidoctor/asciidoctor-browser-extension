5b9a46c016a6142347dfa1fc021d8aaaec968c85
Guillaume Grossetie <ggrossetie@gmail.com>
2018-11-25 16:19:55 +0100

    Upgrade to Asciidoctor.js 1.5.9
    - Upgrade Asciidoctor PlantUML extension to 0.10.0
    - Update the emoji and chart extensions
    - Add the chart title

--- app/js/vendor/asciidoctor-chart-block-macro.js
+++ app/js/vendor/asciidoctor-chart-block-macro.js
@@ -8,29 +8,30 @@ asciidoctor.browser.extensions.Chart.process = function (data, labels, attrs) {
 };
 
 asciidoctor.browser.extensions.Chart.getHeight = function (attrs) {
-  const height = attrs['$[]']('height');
+  const height = attrs.height;
   return typeof height === 'string' ? height : '400';
 };
 
 asciidoctor.browser.extensions.Chart.getWidth = function (attrs) {
-  const width = attrs['$[]']('width');
+  const width = attrs.width;
   return typeof width === 'string' ? width : '600';
 };
 
 asciidoctor.browser.extensions.Chart.getDiv = function (data, labels, attrs) {
   const series = data.map((value, index) => `data-chart-series-${index}="${value.join(',')}"`);
-  return `<div class="ct-chart"
+  const title = attrs.title ? `<div class="title">${attrs.title}</div>\n` : '';
+  return `<div class="openblock">${title}<div class="ct-chart"
                data-chart-height="${this.getHeight(attrs)}"
                data-chart-width="${this.getWidth(attrs)}"
                data-chart-type="${this.getType(attrs)}"
                data-chart-colors="#72B3CC,#8EB33B"
                data-chart-labels="${labels.join(',')}"
                ${series.join(' ')}
-               data-chart-height="${this.getHeight(attrs)}"></div>`;
+               data-chart-height="${this.getHeight(attrs)}"></div></div>`;
 };
 
 asciidoctor.browser.extensions.Chart.getType = function (attrs) {
-  const type = attrs['$[]']('type');
+  const type = attrs.type;
   if (type === 'bar') {
     return 'Bar';
   } else if (type === 'line') {
@@ -51,12 +52,13 @@ processor.Extensions.register(function () {
 
     self.process(function (parent, target, attrs) {
       const filePath = parent.normalizeAssetPath(target, 'target');
-      const fileContent = parent.readAsset(filePath, {'warn_on_failure': true, 'normalize': true});
+      const fileContent = parent.readAsset(filePath, { 'warn_on_failure': true, 'normalize': true });
       const lines = fileContent.split('\n');
       const labels = lines[0].split(',');
       lines.shift();
       const data = lines.map(line => line.split(','));
       const html = asciidoctor.browser.extensions.Chart.process(data, labels, attrs);
+      console.log(attrs);
       return self.createBlock(parent, 'pass', html, attrs, {});
     });
   });
@@ -67,7 +69,7 @@ processor.Extensions.register(function () {
     self.named('chart');
     self.positionalAttributes(['size', 'width', 'height']);
     self.$content_model('raw');
-    self.$contexts('literal');
+    self.onContext('literal');
 
     self.process(function (parent, reader, attrs) {
       const lines = reader.getLines();
